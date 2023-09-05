e48a54497d55177f7da6095dde606cdaaced69d5
Michael Dawson <m.dawson@y7mail.com>
2023-09-05 16:15:38 -0600

    Update profile fields

--- lib/schema/profile.d.ts
+++ lib/schema/profile.d.ts
@@ -2,8 +2,10 @@ import { TimestampStub } from '../utils/TimestampStub';
 export interface ProfileValue {
     createdAt: TimestampStub;
     updatedAt: TimestampStub;
-    ask: string;
-    offer: string;
+    uid: string;
+    helpWanted?: string;
+    canOffer?: string;
+    name: string;
     headline?: string;
     photoStoragePath?: string;
 }
--- src/schema/profile.ts
+++ src/schema/profile.ts
@@ -3,8 +3,10 @@ import { TimestampStub } from '../utils/TimestampStub';
 export interface ProfileValue {
   createdAt: TimestampStub;
   updatedAt: TimestampStub;
-  ask: string;
-  offer: string;
+  uid: string;
+  helpWanted?: string;
+  canOffer?: string;
+  name: string;
   headline?: string;
   photoStoragePath?: string;
 }
